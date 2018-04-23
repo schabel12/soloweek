fa049dc7d9ab2a11f9aba4f7fa798acec65098fc
Ryan Schabel <schabel12@gmail.com>
2018-04-22 23:20:19 -0400

    attempting again

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
-const port = process.env.PORT || 5000;
+const port = 80;
 const axios = require("axios");
 const path = require("path");
 require('dotenv').config();
